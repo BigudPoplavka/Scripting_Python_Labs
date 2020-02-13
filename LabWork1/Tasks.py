d88d5ff2c8042cd18449ae4c4f31665ee3b0ec35
LoliScripter <bogdanpavl2000@mail.ru>
2020-02-13 22:07:53 +0300

    Big refactoring

--- LabWork1/Tasks.py
+++ LabWork1/Tasks.py
@@ -1,15 +1,15 @@
 import math
+import datetime
+import random
+import itertools
 from math import *
 import numbers
 import enum
-import random
 import time
-import datetime
 
-#global variables
 msgEnText = 'Enter the text: '
 
-# Преобразование введенного дробного числа к денежному формату
+# Преобрахование введенного дробного числа к денежному формату
 def Ex1():
     summ = input('Enter sum: ').replace(',', '.').split('.')
     if int(summ[0]) < 0:
@@ -19,169 +19,72 @@ def Ex1():
 
 # Проверка программно задаваемого списка на возрастание
 def Ex2():
-    maxSize = 100
-    i = 0
-    l = []
-
-    while i < maxSize:
-        l.append(int(input('Add elem: ')))
-        if len(l) > 1:
-            if l[i] > l[i - 1]:
-                print('true - flaxiness is increasing')
-            else:
-                print ('false - flaxiness is decreasing')
-                break
-        i += 1
+    flaxiness = [i for i in input('Add numbers: ').split(' ')]
+    print(True) if [i for i in range(len(flaxiness) - 1) if flaxiness[i] <= flaxiness[i + 1]] else print(False)
 
 # Замена символов на * с 4-го и до 4-х последних
 def Ex3():
-    maxLen = 16
-    num = input('Enter card-number: ')
-    passw = '********'
+    MAX_LEN = 16
 
-    if len(num) < maxLen:
-        raise Exception('Incorrect number format!')
-    else:
-        preNum = num[:4]
-        pastNum = num[12:maxLen]
-    print ('Your card-number: {0}{1}{2}'.format(preNum, passw, pastNum))
+    card = input('Enter card-number: ').replace(' ', '').replace('-', '')
+    print(card[:4] + '*' * 8 + card[12:MAX_LEN]) if len(card) == MAX_LEN else print('ERROR! Incorrect format!')
 
 # Разделение текста на слова. Вывод слов длиной > 7, потом от 4 до 7, потом остальных
-def getWords(text):
-    text = text.replace('\n', ' ')
-    text = text.replace('.', '').replace(',', '').replace('?', '').replace('!', '').replace(':', '')
-    words = text.split()
-    return words
-
-def processText(array):
-    print('\nДлины больше 7 символов: \n')
-    for i in range(0, len(array)):
-        if len(array[i]) > 7:
-            print('{0} - {1} символов'.format(array[i], len(array[i])))
-
-    print('\nДлины от 4 до 7 символов:\n')
-    for i in range(0, len(array)):
-        if len(array[i]) > 4 and len(array[i]) < 7:
-            print('{0} - {1} символов'.format(array[i], len(array[i])))
-
-    print('\nОстальные длины слов: \n')
-    for i in range(0, len(array)):
-        if len(array[i]) <= 4:
-            print('{0} - {1} символов'.format(array[i], len(array[i])))
-
 def Ex4():
-    minLen = 4
-    maxLen = 7
-
-    sourse = input(msgEnText)
-    print(processText(getWords(sourse)))
+    text = input(msgEnText).split(' ')
+    print('Length > 7: {0}\n'.format([i for i in text if len(i) > 7]))
+    print('Length 4 - 7: {0}\n'.format([i for i in text if len(i) >= 4 and len(i) <= 7]))
+    print('Length <= 4: {0}\n'.format([i for i in text if len(i) < 4]))
 
 # Привести к верхнему регистру все слова, начинающиеся с заглавной буквы
 def Ex5():
-    sourse = input(msgEnText)
-    words = sourse.split()
-
-    for i in range(len(words)):
-        if words[i] == words[i].title():
-            sourse = sourse.replace(words[i], words[i].upper())
-    print(sourse)
+    sourse = input().split(' ')
+    print(' '.join([i.upper() if i == i.title() else i for i in sourse]))
 
 # Вывести символы, встречающиеся в строке только один раз
 def Ex6():
     sourse = input(msgEnText)
-    count = 0
-
-    for i in range(len(sourse)):
-        if sourse.count(sourse[i]) == 1:
-            count += 1
-            print(sourse[i])
-    print('Всего уникальных символов: {0}'.format(count))
+    print('Unique symbols:', ', '.join([i for i in sourse if sourse.count(i) == 1]))
 
 # Скрипт должен обрабатывать список веб-адресов и проверять
 # начинается ли с 'www.' - если да, то дописать 'http://' и затем
 # проверить окончание на '.com' - если отсутствует - дописать
-# Использовать генераторы списков
+# Испольвать генераторы списков
 def Ex7():
-    www = 'www.'
-    com = '.com'
-    http = 'http://'
     urlList = ['www.facebook.com', 'webpage.com', 'www.vk', 'Animevost.org']
-
-    AddStart = [http + i for i in urlList if(www in i)]
-    AddEnd = [i + com if(not com in i) else i for i in AddStart]
-
-    print(AddEnd)
+    AddStart = ['http://' + i if ('www.' in i) else 'http://www.' + i for i in urlList]
+    print([i + '.com' if (not '.com' in i) else i for i in AddStart])
 
 # Скрипт долен генерировать случайным образом число n от 1 до 10000 и
 # создать массив из n целых чисел и дополнить его до размера ближайшей сверху степенью двойки
 def Ex8():
-    maxRand = 10000
-    maxPow = 14    #т.к. 14-я степень двойки = первому числу больше 10000
-    k = 0
-    n = int(random.uniform(1, maxRand))
-    array = []
+    array = [int(random.uniform(1, 1000)) for i in range(0, int(random.uniform(1, 10000)))]
+    newSize = 2 ** math.ceil(math.log2(len(array)))
 
-    for i in range(0, n):
-        array.append(int(random.uniform(1, maxRand)))
-
-    for i in range(0, maxPow):
-        if 2**i > n:
-            k = 2**i
-            break
-
-    while len(array) != k:
-        array.append(0)
+    print('Size before: {0}\n'.format(len(array)))
+    [array.append(random.uniform(1, 1000)) for i in range(len(array), newSize)]
+    print('Size after: {0}\n'.format(len(array)))
 
 # При вводе от пользователя суммы денег скрипт должен выдать
 # кол-во купюр каждого наименования, иначе сообщение "Операция не может быть выполнена"
 def PrintBank(bank):
     return bank[1000] * 1000 + bank[500] * 500 + bank[100] * 100 + bank[50] * 50 + bank[10] * 10
 
-def printBanknotes(bank):
-      res = ''
-      for i in bank.keys():
-            res = res + str(i) + '*' + str(bank[i]) + '+'
-      print(res)
-
-def Calculating(bank, summ, nominal):
-    _ToTake = int(summ / nominal) // 1
-    _TryTake = nominal * bank[nominal] - _ToTake * nominal
-
-    if _TryTake >= 0:
-        _CanTake = _ToTake * nominal
-        bank[nominal] = _ToTake
-        summ -= _CanTake
-        return _ToTake, int(summ)
-    else:
-        return 0
-
-def GetBanknotes(bank, summ, nominal):
-    if PrintBank(bank) < summ:
-        raise Exception('ERROR! Ammount too big!')
-    newSumm = 0
-    bank[nominal], newSumm = Calculating(bank, summ, nominal)
-
-    return int(newSumm)
+def GetBanknotes(bank, summ):
+    for nominal in bank:
+        _take = int(summ / nominal) // 1 if int(summ / nominal) // 1 <= bank[nominal] else bank[nominal]
+        summ -= _take * nominal
+        bank[nominal] = _take
+    print(str(bank).replace(': ', '*').replace(', ', ' + '))
 
 def Ex9():
     bank = {1000: 20, 500: 10, 100: 10, 50: 10, 10: 10}
-    BANK_MAX = PrintBank(bank)
-    print('Bank: {0}'.format(PrintBank(bank)))
+    print('Bank summ: {0}\nBank: {1}'.format(PrintBank(bank), bank))
 
-    try:
-        money = int(input('Enter summ: '))
-        if money % 10 != 0:
-            raise Exception('ERROR! Impossible to execute!')
-
-        for banknote in bank.keys():
-            money = GetBanknotes(bank, money, banknote)
-
-        print('---------------------\n')
-        printBanknotes(bank)
-        print('Bank: '.format(BANK_MAX - money))
-        print('Your payment: {0}'.format(PrintBank(bank)))
-    except ValueError:
-        print('ERROR! Incorrect input!')
+    money = int(input('Enter summ: '))
+    if money > PrintBank(bank):
+        raise Exception('ERROR! Operation can\'t be executed!')
+    GetBanknotes(bank, money)
 
 # Скрипт, провер\ющий надежность введенного пароля
 # хорошая длина пароля - защита беззащитной лоле =)
@@ -207,12 +110,10 @@ def CheckPassword(passw, maxSize):
     elif veryGoodDef:
         print('Very good defence!\n Password is long enough with different case characters and numbers\n:)')
 
-
 def Ex10():
     MAX_LEN = 12
     password = input('Create password max (size 12): ')
     CheckPassword(password, MAX_LEN)
-
 
 # Написать генератор frange как аналог range с дробным шагом
 def frange(start, end, step):
@@ -247,7 +148,6 @@ def Ex12():
     except ValueError:
         print('ERROR! Incorrect format')
 
-
 # Написать генератор extra_enumerate()
 # пример вызова: for i, elem, sum, frac in extra_enumerate(x): рrint(elem, sum, frac)
 # где sum хранит накопленную сумму на момент текущей итерации i,
@@ -263,7 +163,6 @@ def Ex13():
     x = [1, 3, 4, 2]
     for elem, summ, frac in extra_enumerate(x):
         print(elem, summ, frac)
-
 
 # Написать декоратор non_empty, проверяющий списковый результат любой функции:
 # если в нем содержатся пустые строки или значение None, то они удаляются
@@ -285,7 +184,6 @@ def getList():
 
 def Ex14():
     print(getList())
-
 
 # Напишите параметризированный декоратор pre_process, осуществляющий
 # предварительную обработку (цифровую фильтрацию) списка по алгоритму:
@@ -310,7 +208,6 @@ def Ex15():
     signal = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
     plot_signal(signal)
 
-
 # Напишите скрипт, который на основе списка из 16 названий футбольных команд случайным образом
 # формирует 4 группы по 4 команды, а также выводит на консоль календарь всех игр (игры
 # должны проходить по средам, раз в 2 недели, начиная с 14 сентября текущего года).
@@ -324,42 +221,12 @@ def Ex16():
         'Ливерпуль', 'Интер', 'Марсель', 'Арсенал',
         'Боруссия', 'Челси', 'Лион', 'Фенербахче'
     ]
-    groups = [[], [], [], []]
+    playDate = datetime.datetime(2020, 9, 14, 22, 45)
 
     random.shuffle(teamList)
-    for i in range(0, 16, 4):
-        groups[int(i / 4) - 1] = teamList[i:i + 4]
+    groups = [teamList[i*4:i*4+4] for i in range(4)]
+    [print('Group #:', i+1, ' - ', groups[i]) for i in range(len(groups))]
 
-    for i in range(len(groups)):
-        print('Group #{0}: {1}\n'.format(i + 1, groups[i]))
-
-    playDate = datetime.datetime(2020, 9, 14, 22, 45)
-    match = ''
-    i = 0
-    while i < len(teamList):
-        match += 'MATCH: {1} VS. {0}'.format(teamList[i], teamList[i+1])
-        print(match, playDate.strftime("%d/%m/%Y %H:%M"))
-        match = ''
-        playDate = playDate + datetime.timedelta(days=14)
-        i += 2
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#дней в году
+    for i in range(len(teamList)):
+        print("Game #:", i, playDate.strftime("%d/%m/%Y %H:%M"))
+        playDate += datetime.timedelta(days=14)
