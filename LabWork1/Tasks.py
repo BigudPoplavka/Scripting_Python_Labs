a6c359364203dffc1a20a7ffe364e493b96f3854
Bogdan <50374356+LoliScripter@users.noreply.github.com>
2020-03-21 13:33:43 +0300

    Update Tasks.py

--- LabWork1/Tasks.py
+++ LabWork1/Tasks.py
@@ -7,9 +7,11 @@ import numbers
 import enum
 import time
 
+
 msgEnText = 'Enter the text: '
 
 # Преобразование введенного дробного числа к денежному формату
+
 def Ex1():
     summ = input('Enter sum: ').replace(',', '.').split('.')
     if int(summ[0]) < 0:
@@ -17,46 +19,60 @@ def Ex1():
     else:
         print(summ[0] + ' руб. ' + summ[1] + ' коп.')
 
+        
 # Проверка программно задаваемого списка на возрастание
+
 def Ex2():
     flaxiness = [i for i in input('Add numbers: ').split(' ')]
     print(True) if [i for i in range(len(flaxiness) - 1) if flaxiness[i] <= flaxiness[i + 1]] else print(False)
 
+    
 # Замена символов на * с 4-го и до 4-х последних
+
 def Ex3():
     MAX_LEN = 16
 
     card = input('Enter card-number: ').replace(' ', '').replace('-', '')
     print(card[:4] + '*' * 8 + card[12:MAX_LEN]) if len(card) == MAX_LEN else print('ERROR! Incorrect format!')
 
+    
 # Разделение текста на слова. Вывод слов длиной > 7, потом от 4 до 7, потом остальных
+
 def Ex4():
     text = input(msgEnText).split(' ')
     print('Length > 7: {0}\n'.format([i for i in text if len(i) > 7]))
     print('Length 4 - 7: {0}\n'.format([i for i in text if len(i) >= 4 and len(i) <= 7]))
     print('Length <= 4: {0}\n'.format([i for i in text if len(i) < 4]))
 
+    
 # Привести к верхнему регистру все слова, начинающиеся с заглавной буквы
+
 def Ex5():
     sourse = input().split(' ')
     print(' '.join([i.upper() if i == i.title() else i for i in sourse]))
 
+    
 # Вывести символы, встречающиеся в строке только один раз
+
 def Ex6():
     sourse = input(msgEnText)
     print('Unique symbols:', ', '.join([i for i in sourse if sourse.count(i) == 1]))
 
+    
 # Скрипт должен обрабатывать список веб-адресов и проверять
 # начинается ли с 'www.' - если да, то дописать 'http://' и затем
 # проверить окончание на '.com' - если отсутствует - дописать
 # Испольвать генераторы списков
+
 def Ex7():
     urlList = ['www.facebook.com', 'webpage.com', 'www.vk', 'Animevost.org']
     AddStart = ['http://' + i if ('www.' in i) else 'http://www.' + i for i in urlList]
     print([i + '.com' if (not '.com' in i) else i for i in AddStart])
+    
 
 # Скрипт долен генерировать случайным образом число n от 1 до 10000 и
 # создать массив из n целых чисел и дополнить его до размера ближайшей сверху степенью двойки
+
 def Ex8():
     array = [int(random.uniform(1, 1000)) for i in range(0, int(random.uniform(1, 10000)))]
     newSize = 2 ** math.ceil(math.log2(len(array)))
@@ -65,10 +81,13 @@ def Ex8():
     [array.append(random.uniform(1, 1000)) for i in range(len(array), newSize)]
     print('Size after: {0}\n'.format(len(array)))
 
+    
 # При вводе от пользователя суммы денег скрипт должен выдать
 # кол-во купюр каждого наименования, иначе сообщение "Операция не может быть выполнена"
+
 def PrintBank(bank):
     return bank[1000] * 1000 + bank[500] * 500 + bank[100] * 100 + bank[50] * 50 + bank[10] * 10
+
 
 def GetBanknotes(bank, summ):
     for nominal in bank:
@@ -77,6 +96,7 @@ def GetBanknotes(bank, summ):
         bank[nominal] = _take
     print(str(bank).replace(': ', '*').replace(', ', ' + '))
 
+    
 def Ex9():
     bank = {1000: 20, 500: 10, 100: 10, 50: 10, 10: 10}
     print('Bank summ: {0}\nBank: {1}'.format(PrintBank(bank), bank))
@@ -86,8 +106,10 @@ def Ex9():
         raise Exception('ERROR! Operation can\'t be executed!')
     GetBanknotes(bank, money)
 
+    
 # Скрипт, провер\ющий надежность введенного пароля
 # хорошая длина пароля - защита беззащитной лоле =)
+
 def CheckPassword(passw, maxSize):
     if len(passw) < 1 or len(passw) > maxSize:
         raise Exception('ERROR! Incorrect length of password!')
@@ -110,26 +132,32 @@ def CheckPassword(passw, maxSize):
     elif veryGoodDef:
         print('Very good defence!\n Password is long enough with different case characters and numbers\n:)')
 
+        
 def Ex10():
     MAX_LEN = 12
     password = input('Create password max (size 12): ')
     CheckPassword(password, MAX_LEN)
 
+    
 # Написать генератор frange как аналог range с дробным шагом
+
 def frange(start, end, step):
     while start <= (end - step):
         yield float('{:.1f}'.format(start + step))
         start += step
 
+        
 def Ex11():
     for x in frange(1, 5, 0.1):
         print(x)
 
+        
 # напишите генератор get_frames(), который производит "оконную декомпозицию сигнала"
 # на основе входного списка генерирует набор списков размера size и степенью перекрытия overlap
 # Пример вызова:
 # for frame in get_frames(signal, size=2, overlap=0.5):
 #   print(frame)
+
 def get_frames(signal, size, overlap):
     showLen = int(size * overlap)
     currStep = 0
@@ -138,6 +166,7 @@ def get_frames(signal, size, overlap):
         yield signal[currStep: currStep + size]
         currStep += showLen
 
+        
 def Ex12():
     try:
         slen = int(input('Enter length of signal: '))
@@ -148,24 +177,29 @@ def Ex12():
     except ValueError:
         print('ERROR! Incorrect format')
 
+        
 # Написать генератор extra_enumerate()
 # пример вызова: for i, elem, sum, frac in extra_enumerate(x): рrint(elem, sum, frac)
 # где sum хранит накопленную сумму на момент текущей итерации i,
 # frac хранит долю накопленной суммы от всей суммы: например
 # для списка x = [1, 3, 4, 2] выведет: [1, 1, 0.1]   [3, 4, 0.4]   [4, 8, 0.8]   [2, 10, 1]
+
 def extra_enumerate(x):
     temp = 0
     for i in x:
         temp += i
         yield i, temp, temp/sum(x)
 
+        
 def Ex13():
     x = [1, 3, 4, 2]
     for elem, summ, frac in extra_enumerate(x):
         print(elem, summ, frac)
 
+        
 # Написать декоратор non_empty, проверяющий списковый результат любой функции:
 # если в нем содержатся пустые строки или значение None, то они удаляются
+
 def non_empty(listRetFunction):
     def wrapper():
         returned = listRetFunction()
@@ -178,16 +212,20 @@ def non_empty(listRetFunction):
         return returned
     return  wrapper
 
+
 @non_empty
 def getList():
     return ['chapter1', '', 'contents', '', 'line1']
 
+
 def Ex14():
     print(getList())
 
+    
 # Напишите параметризированный декоратор pre_process, осуществляющий
 # предварительную обработку (цифровую фильтрацию) списка по алгоритму:
 # s[i] = s[i]-a*s[i - 1]. Параметр а можно задать в коде
+
 def pre_process(a):
     def middleDecorator(listRetFunction):
         def wrapper(*args):
@@ -199,21 +237,25 @@ def pre_process(a):
         return  wrapper
     return middleDecorator
 
+
 @pre_process(a=0.95)
 def plot_signal(s):
     for sample in s:
         print(sample)
 
+        
 def Ex15():
     signal = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
     plot_signal(signal)
 
+    
 # Напишите скрипт, который на основе списка из 16 названий футбольных команд случайным образом
 # формирует 4 группы по 4 команды, а также выводит на консоль календарь всех игр (игры
 # должны проходить по средам, раз в 2 недели, начиная с 14 сентября текущего года).
 # команды, а также выводит на консоль календарь всех игр (игры должны проходить по средам, раз в 2 недели,
 # начиная с 14 сентября текущего года). Даты игр необходимо выводить в формате «14/09/2016, 22:45».
 # Используйте модуль random.
+
 def Ex16():
     teamList = [
         'Барселона', 'Реал Мадрид', 'Манчестер Юнайтед', 'Наполи',
@@ -230,3 +272,4 @@ def Ex16():
     for i in range(len(teamList)):
         print("Game #:", i, playDate.strftime("%d/%m/%Y %H:%M"))
         playDate += datetime.timedelta(days=14)
+        
